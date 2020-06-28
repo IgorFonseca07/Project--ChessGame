1ad316f2c60039082401a6d134d4dfd27c2cf155
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-28 19:17:55 -0300

    Project Created

---
+++ Project--ChessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project__ChessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
